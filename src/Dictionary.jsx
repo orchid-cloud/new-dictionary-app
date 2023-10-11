ba233f02e59364b38be2c15023a71aff881e1fee
NZ <n.prokopyk@gmail.com>
2023-10-11 10:02:08 +0200

    dictionary api integration added

--- src/Dictionary.jsx
+++ src/Dictionary.jsx
@@ -1,11 +1,20 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState(null);
+
+  function handleResponse(response) {
+    console.log(response);
+  }
   function search(event) {
     event.preventDefault();
-    alert(`searching for ${keyword}`);
+
+    let apiKey = "5baf2o9bt204cc528f4b02b039af34cd";
+
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
