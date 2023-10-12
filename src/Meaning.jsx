8a9c3003222a15b69f9e5ae909ceee0e8abf26de
NZ <n.prokopyk@gmail.com>
2023-10-12 12:20:59 +0200

    component synonyms aded

--- src/Meaning.jsx
+++ src/Meaning.jsx
@@ -1,12 +1,22 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   console.log(props.meaning);
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
-      <p>{props.meaning.definition}</p>
+      <p>
+        <strong>Definition: </strong>
+        {props.meaning.definition}
+      </p>
+      <br />
+      <strong>Example: </strong>
       <em>{props.meaning.example}</em>
+      <br />
+      <Synonyms synonyms={props.meaning.synonyms} />
+      {/* <strong>Synonyms: </strong>
+      {props.meaning.synonyms} */}
     </div>
   );
 }
